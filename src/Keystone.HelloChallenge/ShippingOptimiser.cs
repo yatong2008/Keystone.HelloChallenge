930fd01ec67e282aeba92b433ae5702815a4ea13
DESKTOP\Yatong <yatong.zhou@gmail.com>
2021-10-03 20:00:06 +1100

    add implementation of SelectBestCarrierAsync

--- src/Keystone.HelloChallenge/ShippingOptimiser.cs
+++ src/Keystone.HelloChallenge/ShippingOptimiser.cs
@@ -2,6 +2,7 @@
 using System.Threading.Tasks;
 using Microsoft.Extensions.Logging;
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using Keystone.HelloChallenge.Support.QuickShip;
 using Keystone.HelloChallenge.Support.SendIt;
@@ -30,7 +31,35 @@ namespace Keystone.HelloChallenge
 
         public async Task<Carrier> SelectBestCarrierAsync(Shipment shipment, CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            decimal quickShipSum = 0;
+            decimal sendItSum = 0;
+            var quickShipShippingClasses = new List<string>();
+            var sendItRateCodes = new List<string>();
+
+            foreach (var shipmentLine in shipment.ShipmentLines)
+            {
+                var quickShippingPrice = await _quickShipApiClient.CalculateShippingPriceAsync(shipmentLine.CubicWeight, shipmentLine.DeclaredValue, cancellationToken);
+                _log.LogDebug($"Calculate QuickShipping from item code: {shipmentLine.ItemCode}, pick shipping class: {quickShippingPrice.ShippingClass}");
+                quickShipShippingClasses.Add(quickShippingPrice.ShippingClass);
+                quickShipSum += quickShippingPrice.Price;
+
+                await foreach (var rate in _sendItApiClient.GetRatesAsync(cancellationToken))
+                {
+                    if (!(rate.MaxCubicWeight >= shipmentLine.CubicWeight) || rate.MaxParcelValue < shipmentLine.DeclaredValue) continue;
+                    _log.LogDebug($"Calculate SendIt from item code: {shipmentLine.ItemCode}, pick rate code: {rate.RateCode}");
+
+                    sendItSum += (rate.PricePerUnitValue * shipmentLine.DeclaredValue + rate.PricePerUnitWeight * (decimal)shipmentLine.CubicWeight);
+                    sendItRateCodes.Add(rate.RateCode);
+                    break;
+                }
+            }
+
+            if (quickShipSum <= sendItSum)
+            {
+                return new Carrier(nameof(_quickShipApiClient), string.Join(", ", quickShipShippingClasses), quickShipSum);
+            }
+
+            return new Carrier(nameof(_sendItApiClient), string.Join(", ", sendItRateCodes), sendItSum);
         }
     }
 }
